3cf6acf68206e752bea7e17b72a3cc494f11a66c
Ekow <edonohue8@users.noreply.github.com>
2021-06-16 14:48:36 -0700

    Initial files plus setting up input type

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+
+  const uploadImage = (files) => {
+    console.log(files[0]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white"
+      }}
+    >
+      <input
+        type="file"
+        onChange={(event) => {
+          uploadImage(event.target.files);
+        }}
+      />
     </div>
   );
 }
