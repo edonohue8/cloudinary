0072a048e80cbfd8384171e9ac82ca3144acc103
Ekow <edonohue8@users.noreply.github.com>
2021-06-24 12:58:17 -0700

    installed axios, set up Axios.post

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
+import Axios from 'axios';
 import './App.css';
 
 function App() {
 
   const uploadImage = (files) => {
-    console.log(files[0]);
-  }
+    // console.log(files[0]);
+    const formData = new FormData()
+    // sending files to formData
+    formData.append("file", files[0])
+    formData.append("upload_preset", "kzdldvww")
+
+    Axios.post("https://api.cloudinary.com/v1_1/dfmqiwt8n/image/upload",
+      formData
+    ).then((response) => {
+      console.log(response);
+    });
+  };
 
   return (
     <div
